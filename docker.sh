9cdd6ec2bea47ad2b3c7c1c43092f0d6b92abe3d
Jason Yu <yuzhijingcheng1996@hotmail.com>
2023-06-12 09:33:21 +0000

    Added firesim with proper configuration files; improved firesim-related scripts

--- docker.sh
+++ docker.sh
@@ -4,7 +4,13 @@
 ./docker-run.sh ./fast-setup.sh
 ./docker-run.sh ./make-sodium.sh
 ./docker-run.sh ./make-musl.sh
-./docker-run.sh ./make.sh
-./docker-run.sh ./make-sdk.sh
-./docker-run.sh ./make.sh image
+if [ -z "$MAKE_FIRESIM" ]; then
+  ./docker-run.sh ./make.sh
+  ./docker-run.sh ./make-sdk.sh
+  ./docker-run.sh ./make.sh image
+else
+  ./docker-run.sh ./make-firesim.sh
+  ./docker-run.sh ./make-sdk.sh
+  ./docker-run.sh ./make-firesim.sh image
+fi
 
--- run-firesim.sh
+++ run-firesim.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+set -e
+
+if [ -z "$FIRESIM_HOME" ]; then
+    FIRESIM_HOME=firesim
+fi
+
+#ln -srnf build/riscv-pk.build/bbl "$FIRESIM_HOME"/deploy/workloads/elasticlave/bbl
+#ln -srnf build/buildroot.build/images/rootfs.ext2 "$FIRESIM_HOME"/deploy/workloads/elasticlave/rootfs.ext2
+
+cd "$FIRESIM_HOME"
+source sourceme-f1-manager.sh
+cd -
+
 firesim launchrunfarm && firesim infrasetup
 firesim runworkload > /dev/null 2>&1 &
 
-sleep 5
+sleep 20
 
-LOG_FILE=firesim/deploy/logs/"$(ls -1t firesim/deploy/logs | head -n1)"
+LOG_FILE="$FIRESIM_HOME"/deploy/logs/"$(ls -1t "$FIRESIM_HOME"/deploy/logs | head -n1)"
 
 IP=$(awk '/Instance IP/ { print $9; exit 0; }' "$LOG_FILE")
 
-ssh $IP screen -r fsim0
+ssh -t $IP screen -r fsim0
 
 
 yes yes | firesim terminaterunfarm
